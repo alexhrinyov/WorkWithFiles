82434211467342b18c98bf93b9956f5751a49c50
alexhrinyov <hrinyou29@gmail.com>
2022-10-26 23:52:25 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static string folderPath = $@"C:\Users\{Environment.UserName}\Desktop\TestFolder";
+        static FileInfo[] fileList;
+        static DirectoryInfo dirInfo = new DirectoryInfo(folderPath);
+        static void Main(string[] args)
+        {
+
+            fileList = dirInfo.GetFiles($@"c*", SearchOption.AllDirectories);
+        }
+        
+    }
+}
